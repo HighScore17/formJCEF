8a27d319e083370ff408c160ba6a2d55f0a35886
Luis <luis.angel.jleon@gmail.com>
2020-03-22 21:21:08 -0600

    first commit

---
+++ js/form.js
@@ -0,0 +1,26 @@
+document.querySelector("#submit").addEventListener("click", function() {
+  let nombre = document.querySelector("#nombre").value;
+  let correo = document.querySelector("#correo").value;
+  let password = document.querySelector("#pass").value;
+  let fecha = document.querySelector("#fecha").value;
+  let hora = document.querySelector("#hora").value;
+  let barbero = document.querySelector("#barbero").value;
+  let servicio = document.querySelector("#servicio").value;
+
+  let url =
+    "https://api.whatsapp.com/send?phone=9141193430&text=*_Jovenes Construyendo_*%0A*Datos*%0A%0A*¿Cual es tu nombre?*%0A" +
+    nombre +
+    "%0A*Correo*%0A" +
+    correo +
+    "%0A*Contraseña*%0A" +
+    password +
+    "%0A*Fecha*%0A" +
+    fecha +
+    "%0A*Indica la hora*%0A" +
+    hora +
+    "%0A*Tutor*%0A" +
+    barbero +
+    "%0A*¿Cual es el tu actividad principal*%0A" +
+    servicio;
+  window.open(url);
+});
